b2be099b3bca35987f9d4d524b91bb8d06795202
Florian Hennig <flo.hennig@gmail.com>
2012-12-15 16:15:28 +0100

    Ncurses based Text User Interface
    Just an example how the Interface could work with "Pictures"

--- Memory_TUI.pro
+++ Memory_TUI.pro
@@ -3,6 +3,9 @@ CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+SOURCES += main.cpp\
+            ../Memory_API/board.cpp\
+            ../Memory_API/card.cpp\
+            ../Memory_API/player.cpp
 
 LIBS += -L/usr/include/ncurses.h -lncurses
--- main.cpp
+++ main.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
+#include <fstream>
 #include <ncurses.h>
-#include <../Memory_API/lib/includes/board.h>
-#include <../Memory_API/lib/includes/player.h>
+#include <../Memory_API/board.h>
+#include <../Memory_API/player.h>
 #include <stdlib.h>
 using namespace std;
 
 int main()
 {
-    initscr();			/* Start curses mode 		  */
-    printw("Hello World !!!");	/* Print Hello World		  */
-    refresh();			/* Print it on to the real screen */
-    getch();			/* Wait for user input */
-    endwin();			/* End curses mode		  */
+    ifstream file;
+    std::string str;
+    initscr();
+//    getch();
+//    while(!file.eof())
+//    {
+//        getline(file, str);
+//        addstr(str.c_str());
+//        printw("\n");
+//    }
+//    file.close();
 
+    file.open("/home/tuxflo/workspace/QT/Memory_TUI/picures/X.txt");
+    int y=0;
+    int x=0;
+    for(int i=0; i<6; i++)
+    {
+        for(int j=0; j<6; j++)
+        {
+            while(!file.eof())
+            {
+                getline(file, str);
+                mvaddstr(y, x, str.c_str());
+                printw("\n");
+                y++;
+            }
+            y=i*9;
+            x+=16;
+
+        file.clear();
+        file.seekg(0, ios::beg);
+        getch();
+
+        }
+        y=(i+1)*9;
+        x=0;
+    }
+    getch();
+    endwin();
 
     return 0;
 }
